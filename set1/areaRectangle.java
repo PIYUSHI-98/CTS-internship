4cbcdef6437d415a4418a7d306e7b1397d148771
PIYUSHI-98 <49339778+PIYUSHI-98@users.noreply.github.com>
2021-04-01 11:29:49 +0530

    Add files via upload

---
+++ set1/areaRectangle.java
@@ -0,0 +1,11 @@
+package set1;
+
+public class areaRectangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int area=2*3;
+		System.out.println("Area of rectangle:"+area);
+	}
+
+}
---
+++ set1/printDataType.java
@@ -0,0 +1,16 @@
+package set1;
+import java.util.*;
+public class printDataType
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Inputs:");
+		int a=sc.nextInt();
+		double b=sc.nextFloat();
+		String c=sc.next();
+		System.out.println("Integer:"+a);
+		System.out.println("Double:"+b);
+		System.out.println("Character:"+c);
+	}
+}
